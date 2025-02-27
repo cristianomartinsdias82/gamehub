dc34a5e26d06db327db3cc04884ba7fd79c48a9b
Cristiano Dias <crisdi@gmail.com>
2025-02-27 16:53:03 -0300

    Eliminate styled-components npm package and all its dependencies from the project

--- src/pages/Content.tsx
+++ src/pages/Content.tsx
@@ -1,12 +1,7 @@
-import styled from "styled-components";
 import SearchFilters from "../components/Content/SearchFilters/SearchFilters";
 import Catalog from "../components/Content/Catalog/Catalog";
 import { Title } from "../models/Title";
-
-const H2 = styled.h2`
-  font-size: 3rem;
-  font-weight: bolder;
-`;
+import { Heading } from "@chakra-ui/react";
 
 interface Props {
   caption: string;
@@ -18,7 +13,9 @@ interface Props {
 const Content = ({ caption, items, isLoading, applySearchFilters }: Props) => {
   return (
     <div>
-      <H2>{caption}</H2>
+      <Heading as="h2" size="6xl">
+        {caption}
+      </Heading>
       <SearchFilters applySearchFilters={applySearchFilters} />
       <Catalog isLoading={isLoading} items={items} />
     </div>
